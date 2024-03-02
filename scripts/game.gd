b899fd73e394702e795c195752036ef62f73ea44
daredakke <77235881+daredakke@users.noreply.github.com>
2024-03-02 14:05:29 +0000

    Pause/title screen UI

--- scripts/game.gd
+++ scripts/game.gd
@@ -13,6 +13,10 @@ const SHAKE_STRENGTH: float = 8.25
 const SHAKE_DECAY_RATE: float = 10
 
 
+var _game_paused: bool:
+	set(new_value):
+		_game_paused = new_value
+		_handle_pause_state()
 var _current_mode: int = Mode.WINDOW_ONE
 var _resolutions: Array[Vector2i] = [
 	Vector2i(640, 360),
@@ -27,6 +31,7 @@ var _shake_strength: float = 0.0:
 		_shake_strength = clampf(value, 0.0, 100.0)
 
 @onready var camera: Camera2D = %Camera
+@onready var pause: Control = %Pause
 @onready var rand = RandomNumberGenerator.new()
 @onready var noise = FastNoiseLite.new()
 
@@ -35,6 +40,7 @@ func _ready() -> void:
 	rand.randomize()
 	noise.seed = randi()
 	noise.frequency = 0.5
+	_game_paused = true
 	
 	_resize_screen(_current_mode)
 
@@ -52,6 +58,19 @@ func _process(delta: float) -> void:
 		_resize_screen(_current_mode)
 	
 	camera.offset = _screen_shake_decay(delta, SHAKE_DECAY_RATE)
+
+
+func _handle_pause_state() -> void:
+	get_tree().paused = _game_paused
+	
+	if _game_paused:
+		pause.show()
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
+		#crosshair.hide()
+	else:
+		pause.hide()
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
+		#crosshair.show()
 
 
 func _resize_screen(mode: int) -> void:

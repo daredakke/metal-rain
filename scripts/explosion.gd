b1a6be32bfb6f96f02f57933bc69740334465101
daredakke <77235881+daredakke@users.noreply.github.com>
2024-03-03 16:56:48 +0000

    Explosions
    They don't damage anything yet.

---
+++ scripts/explosion.gd
@@ -0,0 +1,19 @@
+class_name Explosion
+extends Area2D
+
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var damage_duration: Timer = $DamageDuration
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	animation_player.play("fade_out")
+
+
+func _on_damage_duration_timeout() -> void:
+	collision_shape_2d.queue_free()
+
+
+func _on_animation_finished(_anim_name: StringName) -> void:
+	queue_free()
